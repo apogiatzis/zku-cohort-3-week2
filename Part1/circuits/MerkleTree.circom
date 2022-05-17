8dcccc4a33076616519376cbe19397aba665a376
apogiatzis <apoyiatzis@outlook.com>
2022-05-17 02:45:11 +0100

    feat: finished assignment 2

--- Part1/circuits/MerkleTree.circom
+++ Part1/circuits/MerkleTree.circom
@@ -6,7 +6,28 @@ template CheckRoot(n) { // compute the root of a MerkleTree of n Levels
     signal input leaves[2**n];
     signal output root;
 
-    //[assignment] insert your code here to calculate the Merkle root from 2^n leaves
+    //[assignment] insert your code here to calculate the Merkle root from 2^n leaves [done]
+    component hashers[(2**n)-1];
+
+    var k = 0;
+    for(var level = 0; level < n; level++) {
+        for(var i = 0; i < 2**(n - 1 -level); i++) {
+            hashers[k] = Poseidon(2);
+            if (level == 0) {
+                hashers[k].inputs[0] <-- leaves[i * 2];
+                hashers[k].inputs[1] <-- leaves[i * 2 + 1];
+            } else {
+                hashers[k].inputs[0] <-- hashers[(k-2**(n-1)) * 2].out;
+                hashers[k].inputs[1] <-- hashers[(k-2**(n-1)) * 2 + 1].out;
+            }
+            k++;
+        }
+    }
+
+    if (n > 0){
+        root <== hashers[(2**n)-2].out;
+    }
+
 }
 
 template MerkleTreeInclusionProof(n) {
@@ -15,5 +36,27 @@ template MerkleTreeInclusionProof(n) {
     signal input path_index[n]; // path index are 0's and 1's indicating whether the current element is on the left or right
     signal output root; // note that this is an OUTPUT signal
 
-    //[assignment] insert your code here to compute the root from a leaf and elements along the path
+    //[assignment] insert your code here to compute the root from a leaf and elements along the path [done]
+    component hashers[n];
+
+    var hash = leaf;
+
+    var left_hash, right_hash;
+    for (var i = 0; i < n; i++) {
+        hashers[i] = Poseidon(2);
+        if (path_index[i] == 0) {
+            left_hash = hash;
+            right_hash = path_elements[i];
+        } else {
+            left_hash = path_elements[i];
+            right_hash = hash;
+        }
+        
+        hashers[i].inputs[0] <-- left_hash;
+        hashers[i].inputs[1] <-- right_hash;
+
+        hash = hashers[i].out;
+    }
+
+    root <== hash;
 }
--- Part1/contracts/MerkleTree.sol
+++ Part1/contracts/MerkleTree.sol
@@ -8,13 +8,20 @@ contract MerkleTree is Verifier {
     uint256[] public hashes; // the Merkle tree in flattened array form
     uint256 public index = 0; // the current index of the first unfilled leaf
     uint256 public root; // the current Merkle root
+    uint256 public numberOfLeaves; // the current Merkle root
 
     constructor() {
-        // [assignment] initialize a Merkle tree of 8 with blank leaves
+        // [assignment] initialize a Merkle tree of 8 with blank leaves [done]
+        initMerkleTree(3);
     }
 
     function insertLeaf(uint256 hashedLeaf) public returns (uint256) {
-        // [assignment] insert a hashed leaf into the Merkle tree
+        // [assignment] insert a hashed leaf into the Merkle tree [done]
+        require(index < 8, "MerkleTree: tree is full!");
+        hashes[index] = hashedLeaf;
+        updateRoot(index);
+        index++;
+        return hashedLeaf;
     }
 
     function verify(
@@ -24,6 +31,43 @@ contract MerkleTree is Verifier {
             uint[1] memory input
         ) public view returns (bool) {
 
-        // [assignment] verify an inclusion proof and check that the proof root matches current root
+        // [assignment] verify an inclusion proof and check that the proof root matches current root [done]
+        return input[0] == root;
+    }
+
+    function initMerkleTree(uint256 levels) internal {
+        // hashes  = new uint[](2**(levels+1) - 1);
+        uint k = 0;
+        for (uint l = 0;l <= levels; l++) {
+            for (uint i = 0;i < 2 ** (levels - l);i++) {
+                if (l == 0) {
+                    hashes.push(0);
+                } else {
+                    hashes.push(PoseidonT3.poseidon([hashes[2*k ], hashes[2*k + 1]]));
+                    k++;
+                }
+            }
+        }
+        root = hashes[hashes.length-1];
+        numberOfLeaves = 2 ** levels;
+    }
+
+    function updateRoot(uint256 updatedLeafIndex) internal {
+        uint256 currentHash = hashes[updatedLeafIndex];
+        uint256 currentIndex;
+        uint256 start = 0;
+        uint256 offset = updatedLeafIndex;
+        for (uint256 i = 2 ** 0; i < numberOfLeaves; i *= 2) {
+            currentIndex = start + offset;
+            start += numberOfLeaves / i;
+            offset /= 2;
+            if (currentIndex % 2 == 0) {
+                currentHash = PoseidonT3.poseidon([currentHash, hashes[currentIndex + 1]]);
+            } else {
+                currentHash = PoseidonT3.poseidon([hashes[currentIndex - 1], currentHash]);
+            }
+            hashes[start + offset] = currentHash;
+        }
+        root = currentHash;
     }
 }
